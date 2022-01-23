d95dd13940f3be9e3427380b0fa0c09e6b13b426
Madu Stanley Munachiso <62253488+pro-munachi@users.noreply.github.com>
2022-01-24 00:27:12 +0100

    finished adding rotation

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,11 +1,13 @@
 import { HomeStyle } from './style.js'
 import ImageSlider from '../../components/Slider/ImageSlider.js'
+import ImageRotate from '../../components/ImageRotate/ImageRotate'
 
 const Home = () => {
   return (
     <HomeStyle>
       Home
       <ImageSlider />
+      <ImageRotate />
     </HomeStyle>
   )
 }
