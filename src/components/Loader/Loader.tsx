4cd6c644fa5b5bda0c5dd539119d11e7ae0a81d5
jeffxson <jeffxson20@gmail.com>
2022-01-26 11:59:33 -0500

    loader
    loging

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,24 @@
+import { LoaderStyle } from './style';
+
+
+const Loader = () => {
+    return (
+
+        <LoaderStyle>
+            <div className="body">
+            <div className="container">
+     
+         <div className="ring"></div>
+         <div className="ring"></div>
+         <div className="ring"></div>
+         <p>be patient werey...</p>
+      </div>
+      </div>
+        </LoaderStyle>
+      
+
+
+        )
+    }
+    
+    export default Loader
