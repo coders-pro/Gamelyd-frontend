5f36517cdb78f92490ffdd17ac7e9f3e89ac4368
jeffxson <jeffxson20@gmail.com>
2022-02-03 09:05:39 -0500

    animation
    role

--- src/screens/Home/style.js
+++ src/screens/Home/style.js
@@ -238,4 +238,59 @@ export const HomeStyle = styled.div`
       }
     }
   }
+  margin: 0;
+  padding: 40px 0;
+  color: white;
+
+  .flex-container {
+    display: none;
+    justify-content: space-between;
+    @media (min-width: 999px) {
+      display: flex; 
+     
+  
+    }
+    padding: 0px;
+    margin-left: -10px;
+    text-align: left;
+
+   
+  }
+  .box{
+    
+    width: 18rem;
+    height: 6rem;
+    background-color:  rgba(31,40,51,1);
+    text-align: left;
+    border-radius: 3%;
+    padding-top: 15px;
+    padding-left: 30px;
+    box-shadow: rgba(255, 255, 255, 0.652); 1.95px 1.95px 2.6px;
+
+  }
+ .p1{
+     font-size: 22px;
+  }
+ .p2{
+     font-size: 16px;
+  }
+    
+
+  .flex-containerphone {
+    
+    @media (min-width: 999px) {
+     
+      display: none;
+      justify-content: space-between;
+  
+    }
+    padding: 0px;
+    margin-left: -10px;
+    margin-top: -130px;
+    margin-bottom: 70px;
+    text-align: left;
+
+   
+  }
+
 `
