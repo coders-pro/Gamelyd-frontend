b5e8b8cfaede3c5cadf65c3b511760dfa7fd68f2
Udosinachi <udosipat@gmail.com>
2022-02-18 23:33:11 +0100

    Modal is set

---
+++ src/components/DisplayModal/DisplayModal.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import { Div } from './Style'
+
+const DisplayModal = () => {
+  return (
+    <div>
+      <Div>
+        <section className='sect'>
+          <div className='box'>
+            <h3>Popular Creator Rank List</h3>
+            <div className='list'>
+              <div className='imgbx'>
+                <img src='/images/fifa.jpg' alt='' />
+              </div>
+              <div className='content'>
+                <h2 className='rank'>
+                  <small>#</small>1
+                </h2>
+                <h4>Ricardo Flahjdkl</h4>
+                <p>Digital Artist</p>
+              </div>
+            </div>
+
+            <div className='list'>
+              <div className='imgbx'>
+                <img src='/images/fifa.jpg' alt='' />
+              </div>
+              <div className='content'>
+                <h2 className='rank'>
+                  <small>#</small>2
+                </h2>
+                <h4>Ricardo Flahjdkl</h4>
+                <p>Digital Artist</p>
+              </div>
+            </div>
+            <div className='list'>
+              <div className='imgbx'>
+                <img src='/images/fifa.jpg' alt='' />
+              </div>
+              <div className='content'>
+                <h2 className='rank'>
+                  <small>#</small>3
+                </h2>
+                <h4>Ricardo Flahjdkl</h4>
+                <p>Digital Artist</p>
+              </div>
+            </div>
+            <div className='list'>
+              <div className='imgbx'>
+                <img src='/images/fifa.jpg' alt='' />
+              </div>
+              <div className='content'>
+                <h2 className='rank'>
+                  <small>#</small>4
+                </h2>
+                <h4>Ricardo Flahjdkl</h4>
+                <p>Digital Artist</p>
+              </div>
+            </div>
+            <div className='list'>
+              <div className='imgbx'>
+                <img src='/images/fifa.jpg' alt='' />
+              </div>
+              <div className='content'>
+                <h2 className='rank'>
+                  <small>#</small>5
+                </h2>
+                <h4>Ricardo Flahjdkl</h4>
+                <p>Digital Artist</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </Div>
+    </div>
+  )
+}
+
+export default DisplayModal
