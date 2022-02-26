050a463d4bb1a962488e4ec9698db516785a4920
mcbobby <madumcbobby@yahoo.com>
2022-02-27 00:06:16 +0100

    finish edit user and change password

---
+++ src/components/EditUser/style.js
@@ -0,0 +1,87 @@
+import styled from "styled-components";
+
+export const Style = styled.div`
+.wrapper {
+      width: 450px;
+      background: rgba(0, 0, 0, 0.6);
+      padding: 10px 30px;
+      box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+      margin: 0px auto;
+
+      @media (max-width: 850px) {
+        width: 75%;
+      }
+    }
+
+    .wrapper .input-data {
+      height: 30px;
+      width: 100%;
+      position: relative;
+      margin: 20px 0;
+    }
+    .wrapper .input-data input {
+      height: 100%;
+      width: 100%;
+      border: none;
+      outline: none;
+      font-size: 17px;
+      background: transparent;
+      color: white;
+    }
+    .input-data input:valid ~ label {
+      transform: translateY(-20px);
+      font-size: 16px;
+      font-weight: 500;
+      color: rgb(11, 192, 180);
+    }
+    .wrapper .input-data label {
+      position: absolute;
+      bottom: 4px;
+      left: 0;
+      color: rgba(197, 198, 199, 1);
+      pointer-events: none;
+    }
+    .underline {
+      margin: -4px;
+    }
+    .wrapper .input-data .underline {
+      position: absolute;
+      height: 2px;
+      width: 100%;
+      bottom: 0;
+      background: linear-gradient(
+        273deg,
+        rgba(197, 198, 199, 1) 0%,
+        rgb(11, 192, 180) 25%,
+        rgba(31, 40, 51, 1) 100%
+      );
+    }
+
+    .input-data .underline:before {
+      position: absolute;
+      content: "";
+      height: 100%;
+      width: 100%;
+      background: rgb(11, 192, 180);
+      transform: scaleX(0);
+      transform-origin: center;
+      transition: transform 0.3s ease;
+    }
+
+    .input-data input:focus ~ .underline:before,
+    .input-data input:valid ~ .underline:before {
+      transform: scaleX(1);
+    }
+  }
+  .buty {
+    width: 130px;
+    padding: 8px 8px;
+    margin: 10px auto;
+    display: flex;
+    justify-content: center;
+    background: rgb(11, 192, 180);
+    color: white;
+    border: none;
+    cursor: pointer;
+  }
+`;
--- src/screens/Profile/profile.js
+++ src/screens/Profile/profile.js
@@ -1,117 +1,223 @@
-import { useParams } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { Style } from './style'
-import Navbar from '../../components/NavBar/Navbar'
-import AccountCircleIcon from '@mui/icons-material/AccountCircle'
-import LocationOnIcon from '@mui/icons-material/LocationOn'
-import TwitterIcon from '@mui/icons-material/Twitter'
-import FacebookIcon from '@mui/icons-material/Facebook'
-import LinkedInIcon from '@mui/icons-material/LinkedIn'
-import InstagramIcon from '@mui/icons-material/Instagram'
-import Button from '../../components/Button/InnerButton'
-
+import { useParams } from "react-router-dom";
+import { useState, useRef, useEffect } from "react";
+import axios from "axios";
+import { Style } from "./style";
+import Navbar from "../../components/NavBar/Navbar";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import Button from "../../components/Button/InnerButton";
+import Loader from "../../components/ButtonLoader/ButtonLoader";
+import List from "../../components/List/list";
+import { toast } from "react-toastify";
+import Scroll from "../../components/Scroll/scroll6";
+import Modal from "../../components/Modal/Modal";
+import EditUser from "../../components/EditUser";
+import ChangePassword from "../../components/ChangePassword";
 const Profile = () => {
-  const [user, setUser] = useState([])
+  const [user, setUser] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [active, setActive] = useState("mine");
+  const [myToynys, setMyToynys] = useState([]);
+  const [myCToynys, setMyCToynys] = useState([]);
+  const [refresh, setRefresh] = useState(true);
 
-  const { id } = useParams()
+  const { id } = useParams();
+
+  const modalRef5 = useRef();
+
+  const create = () => {
+    modalRef5.current.open();
+  };
+
+  const close = () => {
+    modalRef5.current.close();
+  };
+
+  const modalRef = useRef();
+
+  const create1 = () => {
+    modalRef.current.open();
+  };
+
+  const close1 = () => {
+    modalRef.current.close();
+  };
+
+  const getUserTourny = (user_name) => {
+    const headers = {
+      "Content-Type": "application/json",
+      token: localStorage.getItem("token"),
+    };
+    try {
+      axios
+        .get(
+          `https://gamelyd.herokuapp.com/tournament/userRegisteredTournaments/${user_name}`,
+          { headers: headers }
+        )
+        .then((res) => {
+          setLoading(false);
+          if (!res.data.hasError) {
+            if (!res.data.tournaments) {
+              setMyToynys([]);
+            } else {
+              setMyToynys(res.data.tournaments);
+            }
+            console.log(res.data.tournaments);
+          } else {
+            toast.error(res.data.message);
+          }
+        });
+
+      axios
+        .get(`https://gamelyd.herokuapp.com/users/tournaments/${id}`, {
+          headers: headers,
+        })
+        .then((res) => {
+          setLoading(false);
+          if (!res.data.hasError) {
+            console.log(res.data.tournaments);
+            if (!res.data.tournaments) {
+              setMyCToynys([]);
+            } else {
+              setMyCToynys(res.data.tournaments);
+            }
+          } else {
+            toast.error(res.data.message);
+          }
+        });
+    } catch (err) {
+      toast.error("Error please try again");
+    }
+  };
 
   useEffect(() => {
     const headers = {
-      'Content-Type': 'application/json',
-      token: localStorage.getItem('token'),
-    }
+      "Content-Type": "application/json",
+      token: localStorage.getItem("token"),
+    };
 
-    axios
-      .get(`https://gamelyd.herokuapp.com/users/${id}`, { headers: headers })
-      .then((res) => {
-        if (!res.data.hasError) {
-          setUser(res.data.user)
-          console.log(res.data.user)
-        } else {
-          // setUser([])
-        }
-      })
-  }, [id])
+    try {
+      axios
+        .get(`https://gamelyd.herokuapp.com/users/${id}`, { headers: headers })
+        .then((res) => {
+          setLoading(false);
+          if (!res.data.hasError) {
+            toast.success(res.data.message);
+            setUser(res.data.user);
+            getUserTourny(res.data.user.user_name);
+            console.log(res.data.user);
+          } else {
+            toast.error(res.data.message);
+          }
+        });
+    } catch (err) {
+      toast.error("Error please try again");
+      setLoading(false);
+    }
+  }, [id]);
 
   return (
     <>
-      <Navbar message='jh' />
+      {loading && <Loader />}
+      <Navbar message="jh" />
       <Style>
-        <div className='top'>
-          <div className='pic'>
+        <div className="top">
+          <div className="pic">
             <AccountCircleIcon
-              style={{ color: 'white' }}
-              sx={{ fontSize: 300 }}
+              style={{ color: "white" }}
+              sx={{ fontSize: 200 }}
             />
           </div>
-          <div className='text'>
-            <div className='name'>
-              <div className='main'>
-                <div className='aha'>{user.first_name}</div>
-                <div className='user'>@{user.user_name}</div>
-                <div className='user'>
-                  {' '}
-                  <LocationOnIcon sx={{ fontSize: 15 }} /> Nigeria
+          <div className="text">
+            <div className="name">
+              <div className="main">
+                <div className="aha">{user.first_name}</div>
+                <div className="user">@{user.user_name}</div>
+                <div className="user">
+                  {" "}
+                  <LocationOnIcon sx={{ fontSize: 15 }} /> {user.Country}
                 </div>
               </div>
-              <div className='socials'>
-                <TwitterIcon className='soc' />
-                <FacebookIcon className='soc' />
-                <LinkedInIcon className='soc' />
-                <InstagramIcon className='soc' />
+              <div className="socials">
+                <TwitterIcon className="soc" />
+                <FacebookIcon className="soc" />
+                <LinkedInIcon className="soc" />
+                <InstagramIcon className="soc" />
               </div>
             </div>
-            <div className='buttons'>
-              <Button>Deactivate</Button>
-              <Button>Edit</Button>
+            <div className="buttons">
+              <div onClick={create1}>
+                <Button>Change Password</Button>
+              </div>
+              <div onClick={create}>
+                <Button>Edit</Button>
+              </div>
               {/* <Button>Button 1</Button> */}
             </div>
           </div>
+
+          <Modal ref={modalRef5}>
+            <button onClick={close} className="close">
+              X
+            </button>
+            <EditUser user={user} />
+          </Modal>
+
+          <Modal ref={modalRef}>
+            <button onClick={close1} className="close">
+              X
+            </button>
+            <ChangePassword user={user} />
+          </Modal>
         </div>
-        <div className='bottom'>
-          <div className='bio'>
-            <div className='head'>Details</div>
-            <div className='bod'>
+
+        <div className="bottom">
+          <div className="bio">
+            <div className="head">Details</div>
+            <div className="bod">
               <table>
                 <tbody>
                   <tr>
-                    <th style={{ padding: '10px', textAlign: 'left' }}>
+                    <th style={{ padding: "10px", textAlign: "left" }}>
                       First Name:
                     </th>
-                    <td style={{ padding: '10px', textAlign: 'left' }}>
+                    <td style={{ padding: "10px", textAlign: "left" }}>
                       {user.first_name}
                     </td>
                   </tr>
                   <tr>
-                    <th style={{ padding: '10px', textAlign: 'left' }}>
+                    <th style={{ padding: "10px", textAlign: "left" }}>
                       Last Name:
                     </th>
-                    <td style={{ padding: '10px', textAlign: 'left' }}>
+                    <td style={{ padding: "10px", textAlign: "left" }}>
                       {user.last_name}
                     </td>
                   </tr>
                   <tr>
-                    <th style={{ padding: '10px', textAlign: 'left' }}>
+                    <th style={{ padding: "10px", textAlign: "left" }}>
                       Username:
                     </th>
-                    <td style={{ padding: '10px', textAlign: 'left' }}>
-                      {user.user_name}
+                    <td style={{ padding: "10px", textAlign: "left" }}>
+                      @{user.user_name}
                     </td>
                   </tr>
                   <tr>
-                    <th style={{ padding: '10px', textAlign: 'left' }}>
+                    <th style={{ padding: "10px", textAlign: "left" }}>
                       Email:
                     </th>
-                    <td style={{ padding: '10px', textAlign: 'left' }}>
+                    <td style={{ padding: "10px", textAlign: "left" }}>
                       {user.email}
                     </td>
                   </tr>
                   <tr>
-                    <th style={{ padding: '10px', textAlign: 'left' }}>
+                    <th style={{ padding: "10px", textAlign: "left" }}>
                       Number:
                     </th>
-                    <td style={{ padding: '10px', textAlign: 'left' }}>
+                    <td style={{ padding: "10px", textAlign: "left" }}>
                       {user.phone}
                     </td>
                   </tr>
@@ -119,16 +225,39 @@ const Profile = () => {
               </table>
             </div>
           </div>
-          <div className='det'>
-            <div className='head'>Bio</div>
-            <div className='bod'>
-              <p>{user.token}</p>
+          <div className="det">
+            <div className="head">
+              <button
+                onClick={() => setActive("mine")}
+                className={`bts ${active === "mine" ? "act" : "inAct"}`}
+              >
+                Joined tounaments
+              </button>{" "}
+              <button
+                onClick={() => setActive("join")}
+                className={`bts ${active === "join" ? "act" : "inAct"}`}
+              >
+                Created tounaments
+              </button>
+            </div>
+            <div className="bod">
+              {active === "mine" && (
+                <Scroll>
+                  <List mode="mine" tourny={myToynys} />{" "}
+                </Scroll>
+              )}
+              {active === "join" && (
+                <Scroll>
+                  {" "}
+                  <List mode="join" tourny={myCToynys} />{" "}
+                </Scroll>
+              )}
             </div>
           </div>
         </div>
       </Style>
     </>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
